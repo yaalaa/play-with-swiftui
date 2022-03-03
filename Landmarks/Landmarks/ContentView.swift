ab241842833b92e14bbb50774b0835ac79f8092c
yaalaa <jonh.kelvin@gmail.com>
2022-03-03 14:40:51 +0300

    L1.S3

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+
+                }
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
             
     }
 }
