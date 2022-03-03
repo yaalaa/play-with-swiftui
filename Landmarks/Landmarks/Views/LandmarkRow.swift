f9597382446318947e56d391bdcaf305e13da88b
yaalaa <jonh.kelvin@gmail.com>
2022-03-03 23:06:39 +0300

    L2.S2

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by lev on 03.03.2022.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 59)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
