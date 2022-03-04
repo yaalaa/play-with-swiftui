23222269ab696ce67d5669cb7a44e54cc40fbb1b
yaalaa <jonh.kelvin@gmail.com>
2022-03-04 17:08:16 +0300

    L2.S6

---
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by lev on 04.03.2022.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircularImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here")
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
--- Landmarks/Landmarks/Views/LandmarksList.swift
+++ Landmarks/Landmarks/Views/LandmarksList.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct LandmarksList: View {
     var body: some View {
-        List(landmarks) { landmark  in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks) { landmark  in
+                NavigationLink{
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
