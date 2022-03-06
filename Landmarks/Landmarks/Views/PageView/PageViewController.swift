cce0c4a362d2b94c8e66047131d091c1c2f67b03
yaalaa <jonh.kelvin@gmail.com>
2022-03-06 22:46:14 +0300

    C4.L1.S3

--- Landmarks/Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Landmarks/Views/PageView/PageViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 struct PageViewController<Page: View> : UIViewControllerRepresentable {
     var pages: [Page]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -22,18 +23,19 @@ struct PageViewController<Page: View> : UIViewControllerRepresentable {
             navigationOrientation: .horizontal
         )
         pageViewController.dataSource = context.coordinator
+        pageViewController.delegate   = context.coordinator
         return pageViewController
     }
     
     func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
         uiViewController.setViewControllers(
-            [context.coordinator.controllers[0]],
+            [context.coordinator.controllers[currentPage]],
             direction: .forward,
             animated : true
         )
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent     : PageViewController
         var controllers: [UIViewController]
         
@@ -57,6 +59,14 @@ struct PageViewController<Page: View> : UIViewControllerRepresentable {
                 return controllers.first
             } else {
                 return controllers[index + 1]
+            }
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index                 = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
             }
         }
     }
