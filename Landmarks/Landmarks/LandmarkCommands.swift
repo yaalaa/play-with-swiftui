2dc35e62adfead6b9739756460aa02ef32d411bb
yaalaa <jonh.kelvin@gmail.com>
2022-03-08 14:35:29 +0300

    C4.L3.S5

---
+++ Landmarks/Landmarks/LandmarkCommands.swift
@@ -0,0 +1,16 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by lev on 08.03.2022.
+//
+
+import SwiftUI
+
+
+struct LandmarkCommands : Commands {
+    
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
